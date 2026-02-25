79bd06757d0b65e74f7fab6e6cbd2c022fbd92f0
JohnmaDev <jhonechavarria0506@gmail.com>
2026-02-24 21:22:52 -0500

    fix: Se cambia nombre de Github y se modifica año Pie de página

--- Front/src/components/icons/P_rose.vue
+++ Front/src/components/icons/P_rose.vue
@@ -2,14 +2,14 @@
     <img src="./p_rose.svg" class="w-32 h-32" />
     <footer class="text-center text-xs text-gray-500 py-6 mt-10">
             <p>
-              &lt;/&gt; 2025 — Built with ❤️ by
+              &lt;/&gt; 2026 — Built with ❤️ by
               <a
                 href="https://www.linkedin.com/in/john-mario-echavarria-bermudez/"
                 target="_blank"
                 rel="noopener noreferrer"
                 class="text-amber-400 font-medium hover:text-amber-300 transition duration-300"
               >
-                Minguitof
+                JohnmaDev
               </a>
               · All rights reserved.
             </p>
